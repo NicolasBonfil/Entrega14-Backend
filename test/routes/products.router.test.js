396e0932e77550247b93adb552dd898abb0a2d9c
Nicolas Bonfil <bonfil.nico@gmail.com>
2023-10-19 18:05:34 -0300

    listo

--- test/routes/products.router.test.js
+++ test/routes/products.router.test.js
@@ -4,10 +4,31 @@ import { dropProducts } from "../setup.test.js"
 
 const requester = supertest("http://localhost:8080")
 
-describe("Products router test case", () => {
-    before(async () => {
+let productId;
+
+describe("Products router test case", async () => {
+    before(async (done) => {
         this.timeout(50000)
         await dropProducts()
+        // done()
+    })
+
+    after(async(done) => {
+        done()
+    })
+
+    it("[GET] api/products -get all products", async () => {
+        const response = await requester.get("/api/products")
+        console.log(response);
+
+        expect(response.statusCode).to.be.eql(200)
+    })
+
+    it("[GET] api/products -get a product by id", async () => {
+        const response = await requester.get(`/api/products/${productId}`)
+        console.log(response);
+
+        expect(response.statusCode).to.be.eql(200)
     })
 
     it("[POST] api/products -create a product", async () => {
@@ -27,5 +48,27 @@ describe("Products router test case", () => {
 
         expect(response.statusCode).to.be.eql(200)
         expect(response.body.payload._id).to.be.ok
+
+        productId = response.body.payload._id
+    })
+
+    it("[PUT] api/products -update a product", async () => {
+        const mockData = {
+            "price": 10,
+            "code": "12210",
+            "stock": 1
+        }
+
+        const response = await requester.put("/api/products").send(mockData)
+        console.log(response);
+
+        expect(response.statusCode).to.be.eql(200)
+    })
+
+    it("[DELETE] api/products -delete a product", async () => {
+        const response = await requester.delete(`/api/products/${productId}`)
+        console.log(response);
+
+        expect(response.statusCode).to.be.eql(200)
     })
 })
